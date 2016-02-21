72b0d805bf504b076242bf6f14e51aed2dcc620b
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-02-21 08:57:18 +0300

    Add base guestbook migrations

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,19 @@
--- name: create-user!
--- creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- name:get-messages
+-- selects all available messages
+SELECT * from guestbook
 
--- name: update-user!
--- update an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
+-- name:get-message
+-- selects one message by id
+SELECT * from guestbook
 WHERE id = :id
 
--- name: get-user
--- retrieve a user given the id.
-SELECT * FROM users
-WHERE id = :id
+-- name:save-message!
+-- creates a new message
+INSERT INTO guestbook
+(name, message, timestamp)
+VALUES (:name, :message, :timestamp)
 
--- name: delete-user!
--- delete a user given the id
-DELETE FROM users
+-- name:delete-message!
+-- delete a message by id
+DELETE from guestbook
 WHERE id = :id
--- test/clj/guestbook/test/db/core.clj
+++ test/clj/guestbook/test/db/core.clj
@@ -11,22 +11,3 @@
   (fn [f]
     (migrations/migrate ["migrate"])
     (f)))
-
-(deftest test-users
-  (jdbc/with-db-transaction [t-conn db/conn]
-    (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-              {:id         "1"
-               :first_name "Sam"
-               :last_name  "Smith"
-               :email      "sam.smith@example.com"
-               :pass       "pass"} {:connection t-conn})))
-    (is (= [{:id         "1"
-             :first_name "Sam"
-             :last_name  "Smith"
-             :email      "sam.smith@example.com"
-             :pass       "pass"
-             :admin      nil
-             :last_login nil
-             :is_active  nil}]
-           (db/get-user {:id "1"} {:connection t-conn})))))
